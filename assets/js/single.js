9b08b4db4ca7015ebe6756adb60d85ff22bd4014
Rhea <rheam97@gmail.com>
2021-12-21 19:05:06 -0500

    added error handling and linked two htmls

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,42 +1,60 @@
+var limitWarningEl = document.querySelector("#limit-warning");
+var queryString = document.location.search
+var repoNameEl = document.querySelector("#repo-name")
+
+var getRepoName = function () {
+    var repoName = queryString.split("=")[1]
+    if (repoName) {
+        repoNameEl.textContent = repoName
+        getRepoIssues(repoName)
+    }
+    else {
+        document.location.replace("./index.html")
+    }
+
+}
 var getRepoIssues = function (repo) {
-    console.log(repo)
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
-    fetch(apiUrl).then(function(response){
-        if(response.ok) {
-            response.json().then(function(data){
+    fetch(apiUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
                 displayIssues(data)
             })
+            // check if api has paginated issues
+            if (response.headers.get("Link")) {
+                displayWarning(repo)
+            }
         }
         else {
-            alert("There was a problem with your request.")
+            document.location.replace("./index.html")
         }
     })
 }
 
 var issueContainerEL = document.querySelector("#issues-container")
 
-var displayIssues = function(issues){
-    if (issues.length===0) {
-        issueContainerEL.textContent="This repo has no open issues."
+var displayIssues = function (issues) {
+    if (issues.length === 0) {
+        issueContainerEL.textContent = "This repo has no open issues."
         return
     }
-    for (var i=0; i<issues.length; i++) {
-        var issueEl =document.createElement("a")
+    for (var i = 0; i < issues.length; i++) {
+        var issueEl = document.createElement("a")
         issueEl.classList = "list-item flex-row justify-space-between align-center"
         issueEl.setAttribute("href", issues[i].html_url)
         issueEl.setAttribute("target", "_blank")
         // create span to hold issue title
-        var titleEl =document.createElement("span")
-        titleEl.textContent=issues[i].title
+        var titleEl = document.createElement("span")
+        titleEl.textContent = issues[i].title
         // append to container
         issueEl.appendChild(titleEl)
-        var typeEl =document.createElement("span")
+        var typeEl = document.createElement("span")
         //check to ssee if its isse or pull request
-        if(issues[i].pull_request){
-            typeEl.textContent="(Pull Request)"
+        if (issues[i].pull_request) {
+            typeEl.textContent = "(Pull Request)"
         }
-        else{
-            typeEl.textContent=("Issue")
+        else {
+            typeEl.textContent = ("Issue")
         }
         //append to container
         issueEl.appendChild(typeEl)
@@ -44,4 +62,17 @@ var displayIssues = function(issues){
     }
 }
 
-getRepoIssues("facebook/react")
+var displayWarning = function (repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "to see more than 30 issues, visit "
+    var linkEl = document.createElement("a")
+    linkEl.textContent = "See more issues on Github.com"
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues")
+    linkEl.setAttribute("target", "_blank")
+
+    //append to warning container
+    limitWarningEl.appendChild(linkEl)
+}
+
+
+getRepoName()
